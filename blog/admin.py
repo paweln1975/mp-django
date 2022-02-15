dde5e4c540190b1eb840b025d70e66cf480342db
paweln1975 <pawel.niedziela@gmail.com>
2022-02-15 08:18:33 +0100

    blog model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Tag, Author, Post
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title", )}
+    list_filter = ("title", "author")
+    list_display = ("id", "title", "author", "post_date")
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("caption",)
+
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+    e_mail = models.EmailField()
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20, null=False)
+
+    def full_name(self):
+        return f"{self.caption}"
+
+    def __str__(self):
+        return self.full_name()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    excerpt = models.CharField(max_length=200, null=False)
+    image_name = models.CharField(max_length=32, null=False)
+    post_date = models.DateField(auto_now=True)
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True, unique=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    tags = models.ManyToManyField(Tag, related_name="tags", blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def full_name(self):
+        return f"{self.title}"
+
+    def __str__(self):
+        return self.full_name()
+
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[self.slug])
